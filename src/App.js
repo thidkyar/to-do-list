ce7c31eb1812476b5c76869179f4d02b3c4d6808
Taranvir Hidkyar <thidkyar@gmail.com>
2019-10-17 16:57:14 -0400

    complete basic to do list - to view and delete items

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+//components
+import Task from './Components/Task/Task.jsx'
+import List from './Components/To-Do-List/List.jsx'
 
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [
+        {
+          id: 1,
+          task: 'walk the dog',
+          description: 'walk da dog lol',
+          status: 'Pending',
+          date: "2019-04-20"
+        },
+        {
+          id: 2,
+          task: 'do some',
+          description: 'lil some some',
+          status: 'Pending',
+          date: "2019-04-20"
+        }
+      ]
+    };
+  }
+  _handleSubmit = e =>{
+    e.preventDefault()
+    let items = this.state.items
+      let item = {
+        id: items.length + 1,
+        task: e.target.task.value,
+        date: e.target.date.value,
+        description: e.target.description.value,
+        status: 'Pending'
+      }
+        items.push(item)
+        this.setState({
+          items: items
+        })
+  }
+  _handleStatus = e => {
+    let items = this.state.items
+    items.forEach(item => {
+      if (parseInt(item.id) === parseInt(e.target.id) && item.status === 'Pending') {
+        item.status = 'Complete'
+      }
+    })
+    this.setState({
+      items: items
+    })
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1 style={{textAlign: 'center'}}> To Do List</h1>
+        <Task items={this._handleSubmit} />
+        <List items={this.state.items} status={this._handleStatus}/>
+      </div>
+    );
+  }
+}
 export default App;
